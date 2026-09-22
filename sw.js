1ee69ef9343eec047299b195a2a8942c25826048
hyeop2da <splendidlife87@gmail.com>
2026-09-22 20:35:36 +0900

    추천 기능 반영을 위한 서비스워커 캐시 갱신

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jigeum-yeogi-matjib-v1';
+const CACHE='jigeum-yeogi-matjib-v2';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
